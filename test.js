b2d4e4c488b0573674d4ed44aab65893fed916b6
Aditi Hebbar <aditi@Aditis-MacBook-Pro.local>
2019-04-04 17:35:21 -0400

    barebone test.js

---
+++ test.js
@@ -0,0 +1,12 @@
+const {
+  sequelize,
+  dataTypes,
+  checkModelName,
+  checkPropertyExists
+} = require('sequelize-test-helpers')
+
+
+const AdmissionOfficerModel = require('./models/AdmissionOfficer.js')
+
+
+
